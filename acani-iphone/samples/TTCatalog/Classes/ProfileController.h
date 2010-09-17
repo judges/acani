8f5b3b9e67275ccaea4990f251b7173db16a9a51
Matt Di Pasquale <liveloveprosper@gmail.com>
2010-09-17 00:19:20 -0400

    remove acani-iphone

--- acani-iphone/samples/TTCatalog/Classes/ProfileController.h
+++
@@ -1,9 +0,0 @@
-#import <Three20/Three20.h>
-#import <UIKit/UIKit.h>
-#import <AddressBook/AddressBook.h>
-#import <AddressBookUI/AddressBookUI.h>
-
-@interface ProfileController : UIViewController <ABNewPersonViewControllerDelegate>{
-}
--(void)showNewPersonViewController;
-@end
--- acani-iphone/samples/TTCatalog/QuartzCore.framework/Headers/QuartzCore.h
+++
@@ -1,6 +0,0 @@
-/* QuartzCore.h
-
-   Copyright (c) 2004 Apple Computer, Inc.
-   All rights reserved. */
-
-#import <QuartzCore/CoreAnimation.h>
